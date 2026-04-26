247436aaf8921d52042736946aae3d737699232d
ianarragona05 <icarragona@up.edu.ph>
2026-04-26 20:36:32 +0800

    Initial commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello JavaFX + Maven + WSL!");
+        Scene scene = new Scene(label, 400, 200);
+
+        stage.setTitle("My First JavaFX App");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
