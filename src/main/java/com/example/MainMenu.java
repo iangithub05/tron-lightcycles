ab809a9e141528010e26a803a7e386b44448eddb
Joy Guevarra <joguevarra5@up.edu.ph>
2026-04-27 19:14:32 +0800

    chore: added initial main menu screen

--- src/main/java/com/example/MainMenu.java
+++ src/main/java/com/example/MainMenu.java
@@ -1,6 +1,16 @@
 package com.example;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.control.Button;
 import javafx.geometry.Pos;
 
@@ -12,16 +22,60 @@ public class MainMenu {
         this.app = app;
     }
 
-    public VBox getView() {
-        Button start = new Button("Start Game");
+    public Pane getView() {
+        Pane layout = new Pane();
+        layout.setBackground(createBackground("/images/background.png"));
+
+		Button multiplayerButton = createButton("/images/button_multiplayer.png", 0.30);
+		multiplayerButton.setOnAction(e -> app.showGame());
+        Button singleplayerButton = createButton("/images/button_singleplayer.png", 0.30);
+		singleplayerButton.setOnAction(e -> app.showGame());
         Button exit = new Button("Exit");
-
-        start.setOnAction(e -> app.showGame());
         exit.setOnAction(e -> System.exit(0));
+        
+        VBox playButtons = new VBox(5, multiplayerButton, singleplayerButton);
+        playButtons.setAlignment(Pos.BOTTOM_LEFT);
 
-        VBox layout = new VBox(20, start, exit);
-        layout.setAlignment(Pos.CENTER);
+        playButtons.setLayoutX(50);
+        playButtons.setLayoutY(375);
 
+        layout.getChildren().addAll(playButtons, exit);
+       
         return layout;
+    }
+
+
+
+    // -- helper methods -----------------------------------------------------
+
+    public static Button createButton(String imagePath, double scale) {
+        Image img = new Image(MainMenu.class.getResource(imagePath).toExternalForm());
+        ImageView view = new ImageView(img);
+
+        view.setFitWidth(img.getWidth() * scale);
+        view.setPreserveRatio(true);
+        view.setSmooth(true);
+
+        Button button = new Button();
+        button.setGraphic(view);
+        button.setStyle("-fx-background-color: transparent");
+        button.setCursor(javafx.scene.Cursor.HAND);
+        button.setOnMouseEntered(e -> view.setOpacity(0.8));
+        button.setOnMouseExited(e -> view.setOpacity(1.0));
+
+        return button;
+    }
+
+    public static Background createBackground(String imagePath) {
+        Image bgImg = new Image(MainMenu.class.getResource(imagePath).toExternalForm());
+        BackgroundImage myBI = new BackgroundImage(
+            bgImg,
+            BackgroundRepeat.NO_REPEAT, 
+            BackgroundRepeat.NO_REPEAT, 
+            BackgroundPosition.CENTER,
+            new BackgroundSize(100, 100, true, true, false, true)
+        );
+
+        return new Background(myBI);
     }
 }
